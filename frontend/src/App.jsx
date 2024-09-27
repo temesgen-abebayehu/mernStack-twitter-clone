acaf59b0b913de9de05a9e19c23e7dae9d67e198
temesgen-abebayehu <temesgensoftware21@gmail.com>
2024-09-27 19:09:12 +0300

    UI design completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/home/HomePage.jsx";
+import LoginPage from "./pages/auth/login/LoginPage.jsx";
+import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
+import Sidebar from "./components/common/Sidebar.jsx";
+import RightPanel from "./components/common/RightPanel.jsx";
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
+import ProfilePage from "./pages/profile/ProfilePage.jsx";
+
+function App() {
+  return (
+    <div className="flex max-w-6xl mx-auto">
+		<Sidebar />
+      <Routes>
+		<Route path="/" element={<HomePage />} />
+		<Route path="/login" element={<LoginPage />} />
+		<Route path="/signup" element={<SignUpPage />} />
+		<Route path="/notifications" element={<NotificationPage />} />
+		<Route path="/profile/:username" element={<ProfilePage />} />
+	  </Routes>
+	  <RightPanel />
+    </div>
+  );
+}
+
+export default App;
